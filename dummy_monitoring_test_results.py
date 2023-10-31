c79f7c6136e22f8a17582dd7d6a5dd292c64b7cb
Dave Trier <DaveTrier@modelopdminsmbp.lan>
2023-10-31 14:24:55 -0600

    changed the name of the input asset

--- dummy_monitoring_test_results.py
+++ dummy_monitoring_test_results.py
@@ -11,7 +11,7 @@ def action(data):
 
 # modelop.metrics
 def metrics(df):
-    with open("model_test_results.json", "r") as f:
+    with open("dummy_model_test_results.json", "r") as f:
         contents = f.read()
     test_results = json.loads(contents)
     yield test_results
